cccfa62f19fd4490853da26075db1faa7b16b921
zhang-heng <zhhkay@gmail.com>
2014-07-27 20:06:54 +0800

    update

---
+++ Aircraft/control.h
@@ -0,0 +1,70 @@
+#include "Arduino.h"
+
+//四轴管脚定义
+#define PIN_FRONT 3
+#define PIN_AFTER 9
+#define PIN_LEFT  10
+#define PIN_RIGHT 11
+//当前的四轴油门值
+#define SET_FRONT 0
+#define SET_AFTER 1
+#define SET_LEFT  2
+#define SET_RIGHT 4
+static int PinsPwm[4];
+//ypr初始化值，用于校正平衡姿态
+#define CORRENT_YAW   0
+#define CORRENT_PITCH 1
+#define CORRENT_ROLL  2
+static int CorrectYPR[3];
+//ypr目标值
+#define CONTROL_YAW   0
+#define CONTROL_PITCH 1
+#define CONTROL_ROLL  2
+static int ControlYPR[3];
+//油门级别
+static int LevelPwm = 0;
+//横向旋转速率
+static int LevelYaw = 0;
+
+//最小启动油门
+#define MIN_LEVEL 130
+
+void SetYaw(int v) //C
+{
+  LevelYaw - v; 
+  analogWrite(SET_FRONT,PinsPwm[SET_FRONT]);
+  analogWrite(SET_AFTER,PinsPwm[SET_AFTER]);  
+}
+
+void SetPitch(int v) //->
+{
+  PinsPwm[SET_FRONT] = LevelPwm + v;
+  PinsPwm[SET_AFTER] = LevelPwm + v;
+  analogWrite(SET_FRONT,PinsPwm[SET_FRONT]);
+  analogWrite(SET_AFTER,PinsPwm[SET_AFTER]);
+}
+
+void SetRoll(int v) //T
+{
+  PinsPwm[SET_LEFT] = LevelPwm + v;
+  PinsPwm[SET_RIGHT] = LevelPwm + v;
+  analogWrite(SET_LEFT,PinsPwm[SET_LEFT]);
+  analogWrite(SET_RIGHT,PinsPwm[SET_RIGHT]);
+}
+
+void SetAllPinPwm()
+{
+  analogWrite(PIN_FRONT, PinsPwm[SET_FRONT]);
+  analogWrite(PIN_AFTER, PinsPwm[SET_AFTER]);
+  analogWrite(PIN_LEFT,  PinsPwm[SET_LEFT]);
+  analogWrite(PIN_RIGHT, PinsPwm[SET_RIGHT]);  
+}
+
+void SetAllValue(int v)
+{
+  PinsPwm[SET_FRONT] = v;
+  PinsPwm[SET_AFTER] = v;
+  PinsPwm[SET_LEFT]  = v;
+  PinsPwm[SET_RIGHT] = v;
+  SetAllPinPwm();
+}
---
+++ Aircraft/pid.h
@@ -0,0 +1,42 @@
+typedef struct PID
+{
+  int SetPoint; //设定目标 Desired Value
+  long SumError; //误差累计
+  double Proportion; //比例常数 Proportional Const
+  double Integral; //积分常数 Integral Const
+  double Derivative; //微分常数 Derivative Const
+  int LastError; //Error[-1]
+  int PrevError; //Error[-2]
+} 
+PID;
+
+static PID sPID;
+static PID *sptr = &sPID;
+
+void IncPIDInit(void)
+{
+  sptr->SumError = 0;
+  sptr->LastError = 0; //Error[-1]
+  sptr->PrevError = 0; //Error[-2]
+  sptr->Proportion = 0; //比例常数 Proportional Const
+  sptr->Integral = 0; //积分常数Integral Const
+  sptr->Derivative = 0; //微分常数 Derivative Const
+  sptr->SetPoint = 0;
+}
+
+int IncPIDCalc(int NextPoint)
+{
+  register int iError, iIncpid; //当前误差
+  iError = sptr->SetPoint - NextPoint; //增量计算
+  iIncpid = sptr->Proportion * iError //E[k]项
+  - sptr->Integral * sptr->LastError //E[k－1]项
+  + sptr->Derivative * sptr->PrevError; //E[k－2]项
+  //存储误差，用于下次计算
+  sptr->PrevError = sptr->LastError;
+  sptr->LastError = iError;
+  //返回增量值
+  return(iIncpid);
+}
+
+
+
