382e1ae56484457b8e0ceb43f400ef0fa448acfa
zhang-heng <zhhkay@gmail.com>
2014-08-14 22:16:54 +0800

    up

--- RemoteControl/RemoteControl.ino
+++ RemoteControl/RemoteControl.ino
@@ -1,15 +1,18 @@
 
 #include <ShiftRegLCD.h>
 
-const byte dataPin  = 10;    // SR Data from Arduino pin 10
-const byte clockPin = 11;    // SR Clock from Arduino pin 11
-const byte enablePin = 12;   // LCD enable from Arduino pin 12
-const byte latchPin = 9;
+const byte clockPin = 2;
+const byte dataOutPin = 3;
+const byte latchLcdPin = 4;
+const byte latchOutPin = 5;
+const byte latchInPin = 6;
+const byte dataInPin  = 7;
 
-ShiftRegLCD srlcd(dataPin, clockPin, enablePin, 2);
+ShiftRegLCD srlcd(dataOutPin, clockPin, latchLcdPin, 2);
 
 void setup()
 {
+  Serial.begin(9600);
   uint8_t pitch[8]  = {0x0, 0x4, 0xe, 0x4, 0x4, 0x4, 0x4, 0x0};
   uint8_t roll[8]  = {0x0, 0x0, 0x0, 0x4, 0x1f, 0x0, 0x0, 0x0};
   uint8_t yaw[8]  = {0x0, 0xe, 0x5, 0x4, 0x4, 0x14, 0xe, 0x0};
@@ -19,10 +22,12 @@ void setup()
   srlcd.createChar(2, yaw);
   srlcd.createChar(3, elevation);
 
-  pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(enablePin, OUTPUT);
-  pinMode(latchPin, OUTPUT);
+  pinMode(latchLcdPin, OUTPUT);
+  pinMode(latchOutPin, OUTPUT);
+  pinMode(latchInPin, OUTPUT);
+  pinMode(dataOutPin, OUTPUT);
+  pinMode(dataInPin, INPUT);
   srlcd.write(0);
   srlcd.write(1);
   srlcd.write(2);
@@ -31,7 +36,6 @@ void setup()
 
 void loop()
 {
-  return;
   srlcd.clear();
 
   for (int r = 0; r < 2; r++)
@@ -40,10 +44,24 @@ void loop()
     {
       srlcd.setCursor(c, r);
       srlcd.print("A");
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, 1 << c % 8);
-      digitalWrite(latchPin, HIGH);
+      digitalWrite(latchOutPin, LOW);
+      shiftOut(dataOutPin, clockPin, MSBFIRST, 1 << c % 8);
+      digitalWrite(latchOutPin, HIGH);
     }
   }
-}
 
+  digitalWrite(latchInPin, LOW);
+  byte c = shiftIn(dataInPin, clockPin, MSBFIRST);
+  digitalWrite(latchInPin, HIGH);
+  c = (shiftIn(dataInPin, clockPin, MSBFIRST) << 7) | c >> 1 ;
+  Serial.print(c & 0x80 ? 1 : 0);
+  Serial.print(c & 0x40 ? 1 : 0);
+  Serial.print(c & 0x20 ? 1 : 0);
+  Serial.print(c & 0x10 ? 1 : 0);
+  Serial.print(c & 0x08 ? 1 : 0);
+  Serial.print(c & 0x04 ? 1 : 0);
+  Serial.print(c & 0x02 ? 1 : 0);
+  Serial.print(c & 0x01 ? 1 : 0);
+  Serial.print("\n");
+  delay(100);
+}
